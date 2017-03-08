69b415fa7e99ce0b99843b9bb6eb496a67d80553
Rishith Reddy <babarishith@gmail.com>
2017-03-08 16:06:45 +0530

    optimised error function

--- sorec.py
+++ sorec.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 
 def norm(R):
 	for i in xrange(len(R)):
@@ -8,10 +8,10 @@ def norm(R):
 	return R
 
 def bound(x):
-	return 1/(1 + numpy.exp(-x))
+	return 1/(1 + np.exp(-x))
 
 def dbound(x):
-	y = numpy.exp(x)
+	y = np.exp(x)
 	return y/pow((1 + y),2)
 
 def bou(R):
@@ -31,13 +31,13 @@ def matrix_factorize(R, C, U, V, Z, K, steps=10000, alpha=0.1, beta=0.001, gamma
 	V = V.T
 	Z = Z.T
 	e = 0
-	# ne = 0
+	ne = 0
 	for step in xrange(steps):
 		ne = 0
 		for i in xrange(len(R)):
 			for j in xrange(len(R[i])):
 				if R[i][j] > 0:
-					y = numpy.dot(U[i,:], V[:,j])
+					y = np.dot(U[i,:], V[:,j])
 					a = bound(y)
 					b = dbound(y)
 					eij = (R[i][j] - a)
@@ -47,58 +47,60 @@ def matrix_factorize(R, C, U, V, Z, K, steps=10000, alpha=0.1, beta=0.001, gamma
 					# 	V[k][j] = V[k][j] + alpha * (b * eij * U[i][k] - beta * V[k][j])
 					U[i,:] = U[i,:] + alpha * (b * eij * V[:,j] - beta * U[i,:])
 					V[:,j] = V[:,j] + alpha * (b * eij * U[i,:] - beta * V[:,j])
-					ne += beta * (U[i,:]*U[i,:] + V[:,j]*V[:,j])
+					ne += beta * (np.dot(U[i,:],U[i,:]) + np.dot(V[:,j],V[:,j]))
+					# print ne, "oh"
 			for j in xrange(len(C[i])):
 				if C[i][j] > 0:
-					y = numpy.dot(U[i,:], Z[:,j])
+					y = np.dot(U[i,:], Z[:,j])
 					a = bound(y)
 					b = dbound(y)
-					jminus = numpy.count_nonzero(C[:,j])
-					iplus = numpy.count_nonzero(C[i,:])
-					weight = numpy.sqrt(jminus/ (iplus+jminus + 0.0))
+					jminus = np.count_nonzero(C[:,j])
+					iplus = np.count_nonzero(C[i,:])
+					weight = np.sqrt(jminus/ (iplus+jminus + 0.0))
 					weight = 1
 					eij = (C[i][j] * weight - a)
-					# ne += gamma * eij * eij
+					ne += gamma * eij * eij
 					# for k in xrange(K):
 					# 	U[i][k] = U[i][k] + alpha * (gamma * b * eij * Z[k][j]) 
 					# 	Z[k][j] = Z[k][j] + alpha * (gamma * b * eij * U[i][k] - beta * Z[k][j])
 					U[i,:] = U[i,:] + alpha * (gamma * b * eij * Z[:,j]) 
 					Z[:,j] = Z[:,j] + alpha * (gamma * b * eij * U[i,:] - beta * Z[:,j])
-						# ne += gamma * Z[k][j] * Z[k][j]
-		# ne = ne * 0.5
-		e = 0
-		for i in xrange(len(R)):
-			for j in xrange(len(R[i])):
-				if R[i][j] > 0:
-					y = numpy.dot(U[i,:], V[:,j])
-					a = bound(y)
-					e = e + pow(R[i][j] - a, 2)
-					for k in xrange(K):
-						e = e + (beta/2) * (pow(U[i][k],2) + pow(V[k][j],2))
-		for i in xrange(len(C)):
-			for j in xrange(len(C[i])):
-				if C[i][j] > 0:
-					y = numpy.dot(U[i,:], Z[:,j])
-					a = bound(y)
-					jminus = numpy.count_nonzero(C[:,j])
-					iplus = numpy.count_nonzero(C[i,:])
-					weight = numpy.sqrt(jminus/ (iplus+jminus + 0.0))
-					weight = 1
-					e = e + (gamma/2)*pow(C[i][j] * weight - a, 2)
-					for k in xrange(K):
-						e = e + (beta/2) * (pow(Z[k][j],2))
-		if e < 0.001:
+					ne += beta * np.dot(Z[:,j] , Z[:,j])
+					# print ne, "ah eij", eij
+		ne = ne * 0.5
+		# e = 0
+		# for i in xrange(len(R)):
+		# 	for j in xrange(len(R[i])):
+		# 		if R[i][j] > 0:
+		# 			y = np.dot(U[i,:], V[:,j])
+		# 			a = bound(y)
+		# 			e = e + pow(R[i][j] - a, 2)
+		# 			for k in xrange(K):
+		# 				e = e + (beta/2) * (pow(U[i][k],2) + pow(V[k][j],2))
+		# for i in xrange(len(C)):
+		# 	for j in xrange(len(C[i])):
+		# 		if C[i][j] > 0:
+		# 			y = np.dot(U[i,:], Z[:,j])
+		# 			a = bound(y)
+		# 			jminus = np.count_nonzero(C[:,j])
+		# 			iplus = np.count_nonzero(C[i,:])
+		# 			weight = np.sqrt(jminus/ (iplus+jminus + 0.0))
+		# 			weight = 1
+		# 			e = e + (gamma/2)*pow(C[i][j] * weight - a, 2)
+		# 			for k in xrange(K):
+		# 				e = e + (beta/2) * (pow(Z[k][j],2))
+		if ne < 0.001:
 			# print e
 			break
 		else:
-			print e
+			print ne
 			pass
-	return U, V.T , Z.T, e
+	return U, V.T , Z.T, ne
 
 
 R = [
 	 [5.0,2,0,3,0,4,0,0],
-	 [4,3,0,0,5,0,0,0],
+	 [4,3,0,0,5.0,0,0,0],
 	 [4,0,2,0,0,0,2,4],
 	 [0,0,0,0,0,0,0,0],
 	 [5,1,2,0,4,3,0,0],
@@ -114,27 +116,27 @@ C = [
 	 [0,0,0,0,0,0],
 	]
 
-R = numpy.array(R)
+R = np.array(R)
 R = norm(R)
-C = numpy.array(C)
+C = np.array(C)
 
 N = len(R)
 M = len(R[0])
 
 K = 5
 
-U = numpy.random.rand(N,K)
-V = numpy.random.rand(M,K)
-Z = numpy.random.rand(N,K)
+U = np.random.rand(N,K)
+V = np.random.rand(M,K)
+Z = np.random.rand(N,K)
 
 nU, nV, nZ, e = matrix_factorize(R, C, U, V, Z, K)
 # print nU, "\n", nV
-nR = numpy.dot(nU,nV.T)
+nR = np.dot(nU,nV.T)
 nnR = bou(nR)
 aR = get(nnR)
 # print nnR, "\n", R
 print aR
-nC = numpy.dot(nU,nZ.T)
+nC = np.dot(nU,nZ.T)
 nnC = bou(nC)
 # print nnC
 # print nC, "\n", C
